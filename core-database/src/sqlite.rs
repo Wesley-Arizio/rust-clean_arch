6b16dac9f5370a474004ba988a22c828369693eb
Wesley-Arizio <wesleyazio26@gmail.com>
2023-05-14 13:16:59 +0000

    feat: first migration version

--- core-database/src/sqlite.rs
+++ core-database/src/sqlite.rs
@@ -24,6 +24,50 @@ impl DatabaseRepository {
                 name TEXT NOT NULL,
                 active BOOLEAN NOT NULL DEFAULT true
             );
+
+            CREATE TABLE admins (
+                id UUID NOT NULL PRIMARY KEY,
+                organization_id UUID NOT NULL,
+                email TEXT NOT NULL UNIQUE,
+                password TEXT NOT NULL,
+                is_default BOOLEAN NOT NULL DEFAULT false,
+                FOREIGN KEY (organization_id) REFERENCES organizations(id)
+            );
+
+            CREATE TABLE sellers (
+                id UUID NOT NULL PRIMARY KEY,
+                organization_id UUID NOT NULL,
+                email TEXT NOT NULL UNIQUE,
+                password TEXT NOT NULL,
+                active BOOLEAN NOT NULL DEFAULT true,
+                created_at INTEGER NO NULL DEFAULT (unixepoch('now')),
+                updated_at INTEGER NO NULL DEFAULT (unixepoch('now')),
+                FOREIGN KEY (organization_id) REFERENCES organizations(id)
+            );
+
+            CREATE TABLE products (
+                id UUID NOT NULL PRIMARY KEY,
+                organization_id UUID NOT NULL,
+                name TEXT NOT NULL,
+                amount INTEGER NOT NULL,
+                price BLOB NOT NULL,
+                password TEXT NOT NULL,
+                created_at INTEGER NO NULL DEFAULT (unixepoch('now')),
+                updated_at INTEGER NO NULL DEFAULT (unixepoch('now')),
+                FOREIGN KEY (organization_id) REFERENCES organizations(id)
+            );
+
+            CREATE TABLE sales (
+                id UUID NOT NULL PRIMARY KEY,
+                product_id UUID NOT NULL,
+                seller_id UUID NOT NULL,
+                amount INTEGER NOT NULL,
+                total_price BLOB NOT NULL,
+                created_at INTEGER NO NULL DEFAULT (unixepoch('now')),
+                updated_at INTEGER NO NULL DEFAULT (unixepoch('now')),
+                FOREIGN KEY (product_id) REFERENCES products(id),
+                FOREIGN KEY (seller_id) REFERENCES sellers(id)
+            );
         ";
         let result: sqlx::sqlite::SqliteQueryResult = sqlx::query(&query).execute(&self.connection).await.map_err(DatabaseError::from)?;
         Ok(result)
