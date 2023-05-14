21d493f1f2d5dd57676d08b597d97641cdab9027
Wesley-Arizio <wesleyazio26@gmail.com>
2023-05-14 17:49:39 +0000

    feat: database repository for products

--- core-database/src/entities/product.rs
+++ core-database/src/entities/product.rs
@@ -1,6 +1,13 @@
 use chrono::{DateTime, Utc};
 use num_bigint::BigUint;
+use sqlx::{Pool, Sqlite};
 use uuid::Uuid;
+
+use crate::traits::{DatabaseError, EntityRepository};
+
+pub enum ProductBy {
+    Id(Uuid),
+}
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct ProductDAO {
@@ -52,6 +59,87 @@ impl From<SqliteProductDAO> for ProductDAO {
             price: BigUint::from_bytes_le(&value.price),
             created_at: value.created_at,
             updated_at: value.updated_at,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct ProductRepository;
+
+#[async_trait::async_trait]
+impl EntityRepository<Sqlite, ProductDAO, ProductDAO, ProductDAO, ProductBy, ProductBy>
+    for ProductRepository
+{
+    async fn insert(db: &Pool<Sqlite>, input: ProductDAO) -> Result<ProductDAO, DatabaseError> {
+        let uuid = Uuid::new_v4();
+        let input = SqliteProductDAO::from(input);
+        sqlx::query_as::<_, SqliteProductDAO>(
+            "INSERT INTO products (id, organization_id, name, description, amount, price) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id, organization_id, name, description, amount, price, created_at, updated_at",
+        )
+        .bind(uuid)
+        .bind(input.organization_id)
+        .bind(input.name)
+        .bind(input.description)
+        .bind(input.amount)
+        .bind(input.price)
+        .fetch_one(db)
+        .await
+        .map(ProductDAO::from)
+        .map_err(DatabaseError::from)
+    }
+
+    async fn get(db: &Pool<Sqlite>, key: ProductBy) -> Result<ProductDAO, DatabaseError> {
+        match key {
+            ProductBy::Id(uuid) => sqlx::query_as::<_, SqliteProductDAO>(
+                "SELECT id, organization_id, name, description, amount, price, created_at, updated_at FROM products WHERE id = $1 LIMIT 1",
+            )
+            .bind(uuid)
+            .fetch_one(db)
+            .await
+            .map(ProductDAO::from)
+            .map_err(DatabaseError::from),
+        }
+    }
+
+    async fn get_all(
+        _db: &Pool<Sqlite>,
+        _key: ProductBy,
+    ) -> Result<Vec<ProductDAO>, DatabaseError> {
+        Err(DatabaseError::NotImplemented)
+    }
+
+    async fn update(
+        db: &Pool<Sqlite>,
+        key: ProductBy,
+        input: ProductDAO,
+    ) -> Result<ProductDAO, DatabaseError> {
+        let input = SqliteProductDAO::from(input);
+        match key {
+            ProductBy::Id(uuid) => {
+                sqlx::query_as::<_, SqliteProductDAO>("UPDATE products SET name = $2, description = $3, amount = $4, price = $5, updated_at = unixepoch('now') WHERE id = $1 RETURNING id, organization_id, name, description, amount, price, created_at, updated_at")
+                    .bind(uuid)
+                    .bind(input.name)
+                    .bind(input.description)
+                    .bind(input.amount)
+                    .bind(input.price)
+                    .fetch_one(db)
+                    .await
+                    .map(ProductDAO::from)
+                    .map_err(DatabaseError::from)
+            }
+        }
+    }
+
+    async fn delete(db: &Pool<Sqlite>, key: ProductBy) -> Result<ProductDAO, DatabaseError> {
+        match key {
+            ProductBy::Id(uuid) => sqlx::query_as::<_, SqliteProductDAO>(
+                "DELETE FROM products WHERE id = $1 RETURNING id, organization_id, name, description, amount, price, created_at, updated_at",
+            )
+            .bind(uuid)
+            .fetch_one(db)
+            .await
+            .map(ProductDAO::from)
+            .map_err(DatabaseError::from),
         }
     }
 }
