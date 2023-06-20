8ecd72581d1754d23181e9a8f118d59c85beb21c
Wesley-Arizio <wesleyazio26@gmail.com>
2023-06-20 01:44:03 +0000

    remove empty line

--- authentication/src/main.rs
+++ authentication/src/main.rs
@@ -1,4 +1,4 @@
-// Banco Core (sqlx Sqlite) -> 
+// Banco Core (sqlx Sqlite) ->
 //      Organization { id, name, active,  }
 //      Admin   -> { id, organization_id, email, password, is_default (true or false) } (Has permission to add or remove other admin but other admin cannot remove the default one)
 //      Seller  -> { id, organization_id, email, password, created_at, active }
@@ -6,7 +6,6 @@
 //      Sales   -> { id, product_id, amount, total_price, salesman_id, created_at, updated_at }
 // Banco Authentication (mongodb) ->
 //      token -> { account_id, api_key, permissions, created_at, updated_at, expires_at }
-
 
 fn main() {
     println!("Hello, world!");
