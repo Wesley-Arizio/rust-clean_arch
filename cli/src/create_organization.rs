4d4308c97a26e2b4ecc6fa6b4e9721594f4e577b
Wesley-Arizio <wesleyazio26@gmail.com>
2023-06-20 01:44:21 +0000

    implement organization name validation

--- cli/src/create_organization.rs
+++ cli/src/create_organization.rs
@@ -1,5 +1,5 @@
 use core_database::{
-    entities::organization::{OrganizationDAO, OrganizationRepository},
+    entities::organization::{OrganizationBy, OrganizationDAO, OrganizationRepository},
     sqlite::DatabaseRepository,
     traits::EntityRepository,
 };
@@ -9,7 +9,15 @@ pub async fn create_organization(
     db: &DatabaseRepository,
     name: String,
 ) -> Result<OrganizationDAO, String> {
-    // TODO - validate organization name before creating it
+    let maybe_organization =
+        OrganizationRepository::try_get(&db.connection, OrganizationBy::Name(name.clone()))
+            .await
+            .map_err(|e| format!("database error: {:#?}", e))?;
+
+    if let Some(_) = maybe_organization {
+        return Err("Organization aready exists".to_string());
+    };
+
     OrganizationRepository::insert(
         &db.connection,
         OrganizationDAO {
